93c27c0c893dd64dda6112c6a9803a35eed9bcb7
Innybae <inibae2005@gmail.com>
2023-02-20 14:36:50 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-
 /**
- * main - Main function of my project 
+ * main - print a line of code using puts
  *
- *
- * Return: return 0 everytime
+ * Return: 0
  */
 int main(void)
 {
-
-        char displayed[52]
-
-	strcpy(displayed, "/"Programming is like building a multilingual puzzle");
-        puts(displayed);
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }	
